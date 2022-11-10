3661ddbf3a52cafdcea0c746e741ac2e06f5bb86
rcshpark <rcshpark@gmail.com>
2022-11-10 18:33:52 +0900

    라우터 페이지 정리

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -3,7 +3,8 @@ import 'package:copum/binding/board_binding.dart';
 import 'package:copum/binding/root_page_binding.dart';
 import 'package:copum/controller/root_page_controller.dart';
 import 'package:copum/screen/agreement.dart';
-import 'package:copum/screen/board_screen.dart';
+import 'package:copum/screen/answer_screen.dart';
+
 import 'package:copum/screen/login_screen.dart';
 import 'package:copum/screen/quill_542ver.dart';
 import 'package:copum/screen/register.dart';
@@ -13,6 +14,7 @@ import 'package:copum/src/board_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../binding/kakao_login_binding.dart';
+import 'package:copum/screen/question_screen.dart';
 
 abstract class Routes {
   static const INTRO = '/'; // 인트로 화면
@@ -24,23 +26,28 @@ abstract class Routes {
   static const SEARCH = '/search'; // 검색 화면
   static const PROFILE = '/profile'; //프로필정보 화면
   static const BOARD = '/board';
+  static const ANSWER = '/answer';
 }
 
 class AppPages {
   static final pages = [
     GetPage(
         name: Routes.BOARD,
-        page: () => const BoardScreen(),
+        page: () => const QuestionScreen(),
         // page: () => const Quill_542ver(),
         binding: BoardBinding()),
-    // GetPage(name: Routes.INTRO, page: () => const StartPage()),
-    // GetPage(
-    //     name: Routes.LOGIN,
-    //     page: () => const LoginScreen(),
-    //     binding: HomeBinding()),
-    // GetPage(
-    //     name: Routes.HOME, page: () => HomeScreen(), binding: BoardBinding()),
+    GetPage(name: Routes.INTRO, page: () => const StartPage()),
+    GetPage(
+        name: Routes.LOGIN,
+        page: () => const LoginScreen(),
+        binding: HomeBinding()),
+    GetPage(
+        name: Routes.HOME, page: () => HomeScreen(), binding: BoardBinding()),
     // GetPage(name: Routes.REGISTER, page: () => const RegisterScreen()),
     // GetPage(name: Routes.AGREEMENT, page: (() => const AgreementScreen())),
+    GetPage(
+        name: Routes.ANSWER,
+        page: () => AnswerScreen(),
+        binding: BoardBinding())
   ];
 }
