99d5c6ef369e2308193fd9746d80acedfcf3874e
rcshpark <rcshpark@gmail.com>
2022-11-14 18:00:05 +0900

    프로필화면ui

--- lib/src/profile.dart
+++ lib/src/profile.dart
@@ -7,8 +7,68 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text('내프로필'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            const CircleAvatar(
+              radius: 80,
+              backgroundColor: Colors.blue,
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '사용자이름',
+                        style: TextStyle(
+                            fontFamily: "Elice",
+                            fontSize: 14,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        '레벨',
+                        style: TextStyle(
+                            fontFamily: "Elice",
+                            fontSize: 14,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        '관심기술',
+                        style: TextStyle(
+                            fontFamily: "Elice",
+                            fontSize: 14,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
