7d3f434b1ef0b3297b27226f5c5bb3e29bf3c8d5
rcshpark <rcshpark@gmail.com>
2022-11-20 16:24:45 +0900

    게시판모델 api

--- lib/api/provider/boardapi.dart
+++ lib/api/provider/boardapi.dart
@@ -6,6 +6,10 @@ import 'package:copum/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_rx/get_rx.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_quill/flutter_quill.dart';
+
+const boardUrl = 'http://10.0.2.2:8000/question/'; //게시판
+// const boardUrl = 'http://localhost:8000/question/';
 
 class BoardApiClient {
   final http.Client httpClient;
@@ -14,15 +18,26 @@ class BoardApiClient {
   dynamic fetchData() async {
     try {
       http.Response response = await http.get(Uri.parse(boardUrl));
-      List<dynamic> body = json.decode(response.body);
+      // List<dynamic> body = json.decode(response.body);
+      List<dynamic> body = jsonDecode(utf8.decode(response.bodyBytes));
       var boardModel =
           body.map((dynamic item) => BoardModel.fromJson(item)).toList();
-      //throw Exception('test exception');
+
       return boardModel;
       //return ResponseModel(boardModel: boardModel);
     } catch (e) {
       //return ResponseModel(exception: e as Exception);
       return null;
     }
-  } // 브레이크 포인트 여기서 멈춤 , 스크린 화면으로 데이터가 안넘어옴 , 여기까지는 데이터가 잘 넘어옴
+  }
 }
+
+
+      // var boardModel = QuillController(
+      //     document: Document.fromJson(body),
+      //     selection: TextSelection.collapsed(offset: 0));
+      //throw Exception('test exception');
+
+      // List<BoardModel> boardModel = body.cast<BoardModel>();
+      // List<BoardModel> boardModel =
+      //     body.map((e) => BoardModel.fromJson(e)).toList();
