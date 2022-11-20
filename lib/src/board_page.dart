b624e1c5201806913f8e5a8a47b57f74870a7df0
rcshpark <rcshpark@gmail.com>
2022-11-20 16:27:25 +0900

    게시판 화면 코드 수정

--- lib/src/board_page.dart
+++ lib/src/board_page.dart
@@ -20,64 +20,71 @@ class BoardPage extends GetView<BoardController> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CategoryMenu(),
-        const SizedBox(
-          height: 20,
-        ),
-        GetX<BoardController>(
-          initState: (state) {
-            Get.find<BoardController>().fetchBoard();
-          },
-          builder: (_) {
-            return _.boardModel.length < 1
-                ? CircularProgressIndicator()
-                : SizedBox(
-                    height: 580,
-                    child: Obx(
-                      () => ListView.separated(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        itemBuilder: (context, index) {
-                          try {
-                            dynamic A = _.boardModel[index].content;
-                            print('aaa2');
-                            var myJSON = jsonDecode(A);
-                            print('bbb: $myJSON');
-                            _controller = QuillController(
-                                document: Document.fromJson(myJSON),
-                                selection: TextSelection.collapsed(offset: 0));
-                            var a = _controller.document.toPlainText();
-                            print('values: $myJSON');
-                            print('$a');
-                            _.boardModel.value[index].content = a;
-                            return _.boardModel[index].content;
-                          } catch (e) {
-                            print(e);
-                          }
-                          // dynamic _controller = QuillController(
-                          // document: Document.fromJson(myJSON),``
-                          // selection: TextSelection.collapsed(offset: 0));
-                          return SingleChildScrollView(
-                            child: PostWidget(
-                              _.boardModel.value[index].title,
-                              _.boardModel.value[index].content,
-                              _.boardModel.value[index].answerCounting,
-                              _.boardModel.value[index].questionCounting,
-                            ),
-                          );
-                        },
-                        separatorBuilder: (context, index) => const Divider(
-                          height: 40,
-                          color: Colors.grey,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          CategoryMenu(),
+          const SizedBox(
+            height: 20,
+          ),
+          GetX<BoardController>(
+            initState: (state) {
+              Get.find<BoardController>().fetchBoard();
+            },
+            builder: (_) {
+              return _.boardModel.length < 1
+                  ? CircularProgressIndicator()
+                  : SizedBox(
+                      height: 580,
+                      child: Obx(
+                        () => ListView.separated(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.vertical,
+                          itemBuilder: (context, index) {
+                            try {
+                              dynamic A = _.boardModel[index].content;
+                              print('aaa2');
+                              var myJSON = jsonDecode(A);
+                              print('bbb: $myJSON');
+                              _controller = QuillController(
+                                  document: Document.fromJson(myJSON),
+                                  selection:
+                                      TextSelection.collapsed(offset: 0));
+                              var a = _controller.document.toPlainText();
+                              // print('values: $myJSON');
+                              // print('$a');
+                              _.boardModel.value[index].content = a;
+                              // return _.boardModel[index].content;
+                            } catch (e) {
+                              print(e);
+                            }
+                            // dynamic _controller = QuillController(
+                            // document: Document.fromJson(myJSON),``
+                            // selection: TextSelection.collapsed(offset: 0));
+                            return SingleChildScrollView(
+                              child: PostWidget(
+                                _.boardModel.value[index].title,
+                                _.boardModel.value[index].content,
+                                _.boardModel.value[index].answerCounting,
+                                _.boardModel.value[index].questionCounting,
+                              ),
+                            );
+                          },
+                          separatorBuilder: (context, index) => const Divider(
+                            height: 40,
+                            color: Colors.grey,
+                          ),
+                          itemCount: _.boardModel.value.length,
                         ),
-                        itemCount: _.boardModel.value.length,
-                      ),
-                    ));
-          },
-        ),
-      ],
+                      ));
+            },
+          ),
+        ],
+      ),
     );
   }
 }
