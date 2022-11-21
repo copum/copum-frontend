d7f11a7a4d858715b7fa46cf788d9a9bd72292b5
rcshpark <rcshpark@gmail.com>
2022-11-21 23:03:14 +0900

    답변 화면

--- lib/screen/answer_screen.dart
+++ lib/screen/answer_screen.dart
@@ -14,7 +14,8 @@ import '../src/Search.dart';
 import '../src/board_page.dart';
 import '../src/profile.dart';
 import 'package:get/get.dart';
-import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill/flutter_quill.dart' hide Text;
+import '../widget/answer_widget.dart';
 
 class AnswerScreen extends GetView<BoardController> {
   QuillController _controller = QuillController.basic();
@@ -26,67 +27,17 @@ class AnswerScreen extends GetView<BoardController> {
         appBar: AppBar(
           backgroundColor: Colors.black,
           centerTitle: true,
-          // title: const Text(
-          //   '답변',
-          //   style: TextStyle(fontFamily: 'Elice', fontSize: 24),
-          // ),
+          title: const Text(
+            '답변',
+            style: TextStyle(fontFamily: 'Elice', fontSize: 24),
+          ),
         ),
         body: Column(
           children: [
             const SizedBox(
               height: 50,
             ),
-            GetX<BoardController>(
-              initState: (state) {
-                Get.find<BoardController>().fetchBoard();
-              },
-              builder: (_) {
-                return _.boardModel.length < 1
-                    ? CircularProgressIndicator()
-                    : SizedBox(
-                        child: Obx(
-                        () => ListView.separated(
-                          shrinkWrap: true,
-                          scrollDirection: Axis.vertical,
-                          itemBuilder: (context, index) {
-                            try {
-                              dynamic A = _.boardModel[index].content;
-                              print('aaa2');
-                              var myJSON = jsonDecode(A);
-                              print('bbb: $myJSON');
-                              _controller = QuillController(
-                                  document: Document.fromJson(myJSON),
-                                  selection:
-                                      TextSelection.collapsed(offset: 0));
-                              var a = _controller.document.toPlainText();
-                              // print('values: $myJSON');
-                              // print('$a');
-                              _.boardModel.value[index].content = a;
-                              return _.boardModel[index].content;
-                            } catch (e) {
-                              print(e);
-                            }
-                            // dynamic _controller = QuillController(
-                            // document: Document.fromJson(myJSON),``
-                            // selection: TextSelection.collapsed(offset: 0));
-                            return SingleChildScrollView(
-                              child: PostWidget(
-                                _.boardModel.value[index].title,
-                                _.boardModel.value[index].content,
-                                _.boardModel.value[index].answerCounting,
-                                _.boardModel.value[index].questionCounting,
-                              ),
-                            );
-                          },
-                          separatorBuilder: (context, index) => const Divider(
-                            height: 40,
-                            color: Colors.grey,
-                          ),
-                          itemCount: _.boardModel.value.length,
-                        ),
-                      ));
-              },
-            )
+            AnswerWidget()
           ],
         ));
   }
