0f9bd4002105a7da81fb62358843eecfe8176f07
rcshpark <rcshpark@gmail.com>
2022-11-28 16:34:39 +0900

    answer controller

---
+++ lib/controller/answer_controller copy.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:copum/api/provider/kakao_login_api_client.dart';
+import 'package:copum/api/repository/answer_repository.dart';
+import 'package:copum/api/repository/board_repository.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import '../api/model/boardmodel.dart';
+
+class AnswerController extends GetxController {
+  final AnswerRepository repository;
+  AnswerController({required this.repository});
+
+  var answerModel = [].obs;
+
+  // repository.fetchBoard().then((data) {
+  //   print('boardList size: ${data?.length}');
+  //   boardList = data;
+  // });
+
+  void fetchBoard() async {
+    answerModel.value = await repository.fetchBoard(); //.value
+    // print('ret: ${boardModel.value}');
+  }
+}
