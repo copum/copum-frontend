c3bfba5b47e2d020e0ebb7bf64f154796cf77f73
rcshpark <rcshpark@gmail.com>
2022-11-28 16:34:28 +0900

    answer binding

---
+++ lib/binding/answer_binding.dart
@@ -0,0 +1,22 @@
+import 'package:copum/api/provider/answerapi.dart';
+import 'package:copum/api/provider/boardapi.dart';
+import 'package:copum/api/provider/kakao_login_api_client.dart';
+import 'package:copum/api/repository/answer_repository.dart';
+import 'package:copum/api/repository/board_repository.dart';
+import 'package:copum/api/repository/kakao_login_repository.dart';
+import 'package:copum/controller/answer_controller%20copy.dart';
+import 'package:copum/controller/kakao_login_controller.dart';
+import 'package:copum/controller/board_controller.dart';
+import 'package:http/http.dart' as http;
+import 'package:get/get.dart';
+
+class AnswerBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<AnswerController>(() {
+      return AnswerController(
+          repository: AnswerRepository(
+              apiClient: AnswerApiClient(httpClient: http.Client())));
+    });
+  }
+}
